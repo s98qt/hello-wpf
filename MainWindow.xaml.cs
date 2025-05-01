73773d715e1e45523825695f8dcadc9f8ec05415
s98qt <1937918060@qq.com>
2025-05-01 12:02:58 +0800

    添加项目文件。

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace hello_wpf
+{
+    /// <summary>  
+    /// Interaction logic for MainWindow.xaml  
+    /// </summary>  
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Task.Factory.StartNew(() =>
+            {
+                Task.Delay(3000).Wait();
+
+                // Fix: Use the correct instance of the Button control  
+                button.Dispatcher.Invoke(() =>
+                {
+                    button.Content = "www.wpfsoft.com";
+                });
+            });
+        }
+    }
+}
