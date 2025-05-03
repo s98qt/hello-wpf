3e9a124d2e9e7a24392a5bb08f15266ffcf1b5c9
SQT <sqt@tztek.com>
2025-05-03 22:54:41 +0800

    样式里面的trigger单触发器

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -19,34 +19,38 @@ namespace hello_wpf
         public MainWindow()
         {
             InitializeComponent();
-            Task.Factory.StartNew(() =>
-            {
-                Task.Delay(3000).Wait();
+
+            //#region 异步线程
+            //Task.Factory.StartNew(() =>
+            //{
+            //    Task.Delay(3000).Wait();
 
 
-                // Fix: Use the correct instance of the Button control  
-                //button.Dispatcher.Invoke(() =>
-                //{
-                //    button.Content = "www.wpfsoft.com";
-                //});              
-            });
+            //    // Fix: Use the correct instance of the Button control  
+            //    //button.Dispatcher.Invoke(() =>
+            //    //{
+            //    //    button.Content = "www.wpfsoft.com";
+            //    //});              
+            //});
 
-            // 示例1：使用.Result属性
-            Task<int> task0 = CalculateSomethingAsync();
-            int result0 = task0.Result; // 注意：这会阻塞当前线程直到任务完成
+            //// 示例1：使用.Result属性
+            //Task<int> task0 = CalculateSomethingAsync();
+            //int result0 = task0.Result; // 注意：这会阻塞当前线程直到任务完成
 
-            // 示例3：使用GetAwaiter().GetResult()
-            Task<int> task1 = CalculateSomethingAsync();
-            int result1 = task1.GetAwaiter().GetResult(); // 也会阻塞当前线程
+            //// 示例3：使用GetAwaiter().GetResult()
+            //Task<int> task1 = CalculateSomethingAsync();
+            //int result1 = task1.GetAwaiter().GetResult(); // 也会阻塞当前线程
 
-            // 示例2：使用await（推荐方式）
-            async void SomeMethod()
-            {
-                Task<int> task = CalculateSomethingAsync();
-                int result = await task; // 不会阻塞UI线程
-            }
+            //// 示例2：使用await（推荐方式）
+            //async void SomeMethod()
+            //{
+            //    Task<int> task = CalculateSomethingAsync();
+            //    int result = await task; // 不会阻塞UI线程
+            //}
 
-           
+            //#endregion
+
+
         }
 
         // 示例异步方法
